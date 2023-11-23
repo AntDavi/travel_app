63cdb689b9ab70d5977f6d1db699d74a29f375b2
AntDavi <ant.davi07@gmail.com>
2023-11-23 15:11:57 -0300

    adjustments menu mobile, hero section complete

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,14 +20,13 @@ const Navbar = () => {
         <nav className="flexBetween max-container padding-container relative z-300 py-5">
             <Link href="/">
                 <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
-
             </Link>
 
             {/* Lista de links de navegação na versão desktop */}
             <ul className="hidden h-full gap-12 lg:flex">
                 {NAV_LINKS.map((link) => (
                     // Cada item da lista é um link de navegação
-                    <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCnter cursor-pointer pb-1.5 transition-all hover:font-bold">
+                    <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
                         {/* O conteúdo do link é o rótulo do link */}
                         {link.label}
                     </Link>
@@ -44,30 +43,27 @@ const Navbar = () => {
             </div>
 
             {/* Ícone de menu */}
-            <div onClick={toggleMenu}>
+            <div onClick={toggleMenu} className="inline-block cursor-pointer lg:hidden">
                 {/* <div> */}
                 <Image
                     src="menu.svg"
                     alt="menu"
                     width={32}
                     height={32}
-                    className="inline-block cursor-pointer lg:hidden"
                 />
             </div>
 
             {/* Menu lateral */}
             {menuOpen && (
-                <div className="fixed top-0 right-0 h-full w-64 bg-slate-300 z-50 p-5 flex items-center justify-between flex-col">
+                <div className="fixed top-0 right-0 h-full w-64 bg-slate-100 z-50 p-5 flex items-center justify-between flex-col">
                     <div onClick={toggleMenu}>
                         <Image src="close-black.svg" alt="close" width={32} height={32} />
                     </div>
                     <ul className="">
                         {NAV_LINKS.map((link) => (
-                            <li key={link.key} className="mt-3 mb-5 text-center">
-                                <Link href={link.href}>
-                                    {link.label}
-                                </Link>
-                            </li>
+                            <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold mb-5">
+                                {link.label}
+                            </Link>
                         ))}
                     </ul>
                     <Button
@@ -77,6 +73,13 @@ const Navbar = () => {
                         variant="btn_dark_green"
                     />
                 </div>
+            )}
+            {/* Overlay */}
+            {menuOpen && (
+                <div
+                    className="fixed top-0 left-0 w-full h-full bg-black opacity-70 z-40"
+                    onClick={toggleMenu}
+                ></div>
             )}
         </nav>
     )
